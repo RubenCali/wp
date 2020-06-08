7186da908f442bd06c3b58a316435b668d282ceb
Ruben Cali <48370133+RubenCali@users.noreply.github.com>
2020-06-08 16:21:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    if( have_posts()):
+        while(have_posts()) : the_post(); ?>
+            <h3><?php the_title(); ?></h3>
+            <div><?php the_content(); ?></div>
+            <?php
+        endwhile;
+
+        else: ?>
+    <p>geen berichten gevonden</p>
+    <?php
+
+    endif;
+
+?>
