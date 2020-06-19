3b7b20c1e2d09733cfbfc27360248ce434dd5391
Ruben Cali <48370133+RubenCali@users.noreply.github.com>
2020-06-19 12:07:32 +0200

    Add files via upload

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 function laadStijlBlad(){
     wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+
 }
 add_action('init', 'laadStijlBlad');
 
@@ -12,5 +14,13 @@ function registreerMenu(){
     );
     register_nav_menus();
 }
-add_action('init', 'registreetMenu');
+add_action('init', 'registreerMenu');
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'init', 'register_navwalker' );
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
         while(have_posts()) : the_post(); ?>
             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <div><?php the_content(); ?></div>
-            <div><?php the_excerpt(); ?></div>
             
             <?php endwhile; else: ?>
     <p>geen berichten gevonden</p>
